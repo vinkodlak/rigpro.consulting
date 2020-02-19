1fb827e984385e409b5355308115db3fa665abcb
Vinko Vacek <vinko.vacek@gmail.com>
2020-02-19 16:54:03 +0100

    admin preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,6 +5,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 // import AboutPagePreview from './preview-templates/AboutPagePreview'
 // import BlogPostPreview from './preview-templates/BlogPostPreview'
 // import IndexPagePreview from './preview-templates/IndexPagePreview'
+import OurWorkflowPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
@@ -13,3 +14,4 @@ CMS.registerMediaLibrary(cloudinary)
 // CMS.registerPreviewTemplate('about', AboutPagePreview)
 // CMS.registerPreviewTemplate('products', ProductPagePreview)
 // CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('our-workflow', OurWorkflowPagePreview)
